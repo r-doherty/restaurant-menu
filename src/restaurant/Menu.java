be68b24526701aa75ed8b98637dbd178aff588b6
Ryan Doherty <rjd.atx@gmail.com>
2020-01-11 14:33:28 -0600

    added last updated and *NEW* status property, not a perfect fix but okay

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,16 +1,6 @@
 package restaurant;
 
-import javax.management.MalformedObjectNameException;
-import javax.swing.*;
-import java.util.HashMap;
-import java.util.Date;
-import java.util.ArrayList;
-
-
 public class Menu {
-
-    private Date currentTime = new java.util.Date();
-
 
     public static void main(String[] args)
     {
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,26 +1,29 @@
 package restaurant;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.sql.Timestamp;
 import java.util.Date;
-import java.util.HashMap;
+import java.io.File;
+import java.text.SimpleDateFormat;
 
 public class MenuItem {
+
+    private static Date currentDate = new Date();
+    private static Timestamp currentTs = new Timestamp(currentDate.getTime());
 
     private String name;
     private double price;
     private String description;
     private String category;
-    private Date dateAdded;
+    private String status;
 
-    public MenuItem (String name, double price, String description, String category)
+    public MenuItem (String name, double price, String description, String category, String status)
     {
         this.name = name;
         this.price = price;
         this.description = description;
         this.category = category;
-//        this.dateAdded = dateAdded;
-
+        this.status = status;
     }
 
     public String getName() {return name;}
@@ -35,19 +38,18 @@ public class MenuItem {
     private void setCategory(String aCategory) {category = aCategory;}
 //    private void setDateAdded(Date aDate) {dateAdded = aDate;}
 
-    public static MenuItem banhMi = new MenuItem("Banh Mi", 8.99, "Pork, Chicken, Tofu, or Mushroom on a Toasted Vietnamese Baguette topped with our truck made pickled carrots & daikon, cucumbers, cilantro, jalapenos, and our addicting roasted garlic Sriracha mayo", "Main Course");
-    public static MenuItem kickinChicken = new MenuItem("Kicken Chicken", 8.99, "chicken carnitas, queso fresco, Korean bbq sauce, cucumber, pickled onions, radish, candied jalapeños, sriracha crema", "Main Course");
-    public static MenuItem freshOffTheBoat = new MenuItem("Fresh off the Boat", 9.99, "blue crab cakes, sesame slaw, cucumber, spicy mayo, sesame seeds, pickled onions", "Main Course");
+    public static MenuItem banhMi = new MenuItem("Banh Mi", 8.99, "Pork, Chicken, Tofu, or Mushroom on a Toasted Vietnamese Baguette topped with our truck made pickled carrots & daikon, cucumbers, cilantro, jalapenos, and our addicting roasted garlic Sriracha mayo", "Main Course", "New");
+    public static MenuItem kickinChicken = new MenuItem("Kicken Chicken", 8.99, "chicken carnitas, queso fresco, Korean bbq sauce, cucumber, pickled onions, radish, candied jalapeños, sriracha crema", "Main Course", "Old");
+    public static MenuItem freshOffTheBoat = new MenuItem("Fresh off the Boat", 9.99, "blue crab cakes, sesame slaw, cucumber, spicy mayo, sesame seeds, pickled onions", "Main Course", "New");
 
-    public static MenuItem kimchiFries = new MenuItem("Kimchi Fries", 5.99, "your choice of protein, caramelized kimchi, cheddar + monterey jack, onions, cilantro, magic sauce, sesame seeds, sriracha on a crispy bed of french fries", "Appetizer");
-    public static MenuItem patatasBravas = new MenuItem("Patatas Bravas", 3.99, "potatoes, kimchi, green onions, cilantro, spicy mayo — ++ short ribs $2.00, pork $2.00, egg $1.50", "Appetizer");
+    public static MenuItem kimchiFries = new MenuItem("Kimchi Fries", 5.99, "your choice of protein, caramelized kimchi, cheddar + monterey jack, onions, cilantro, magic sauce, sesame seeds, sriracha on a crispy bed of french fries", "Appetizer", "Old");
+    public static MenuItem patatasBravas = new MenuItem("Patatas Bravas", 3.99, "potatoes, kimchi, green onions, cilantro, spicy mayo — ++ short ribs $2.00, pork $2.00, egg $1.50", "Appetizer", "New");
 
-    public static MenuItem blackWalnutPie = new MenuItem("Black Walnut Pie (Slice)", 2.99, "Decadent, chocolatey, and oh-so-tasty. Grandmothers Texas sheet cake combines rich, chocolate sheet cake with fudgy chocolate-nut icing", "Dessert");
+    public static MenuItem blackWalnutPie = new MenuItem("Black Walnut Pie (Slice)", 2.99, "Decadent, chocolatey, and oh-so-tasty. Grandmothers Texas sheet cake combines rich, chocolate sheet cake with fudgy chocolate-nut icing", "Dessert", "Old");
 
     public static ArrayList<MenuItem> menu = new ArrayList<>();
 
     private static void loadMenu() {
-
         menu.add(banhMi);
         menu.add(kickinChicken);
         menu.add(freshOffTheBoat);
@@ -61,7 +63,12 @@ public class MenuItem {
 
         System.out.println("***Appetizers***");
         for (MenuItem item : menu) {
-            if (item.category == "Appetizer") {
+            if (item.category == "Appetizer" && item.status == "New") {
+                System.out.println(item.name + " — $" + item.price + " *NEW*");
+                System.out.println(" ... " + item.description);
+            }
+            else if (item.category == "Appetizer" && item.status == "Old")
+            {
                 System.out.println(item.name + " — $" + item.price);
                 System.out.println(" ... " + item.description);
             }
@@ -69,7 +76,12 @@ public class MenuItem {
 
         System.out.println("\n***Main Courses***");
         for (MenuItem item : menu) {
-            if (item.category == "Main Course") {
+            if (item.category == "Main Course" && item.status == "New") {
+                System.out.println(item.name + " — $" + item.price + " *NEW*");
+                System.out.println(" ... " + item.description);
+            }
+            else if (item.category == "Main Course" && item.status == "Old")
+            {
                 System.out.println(item.name + " — $" + item.price);
                 System.out.println(" ... " + item.description);
             }
@@ -77,10 +89,20 @@ public class MenuItem {
 
         System.out.println("\n***Desserts***");
         for (MenuItem item : menu) {
-            if (item.category == "Dessert") {
+            if (item.category == "Dessert" && item.status == "New") {
+                System.out.println(item.name + " — $" + item.price + " *NEW*");
+                System.out.println(" ... " + item.description);
+            }
+            else if (item.category == "Dessert" && item.status == "Old")
+            {
                 System.out.println(item.name + " — $" + item.price);
                 System.out.println(" ... " + item.description);
             }
         }
+
+        System.out.println();
+        File file = new File("../RestaurantMenu/src/restaurant/MenuItem.java");
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        System.out.println("Menu last updated : " + sdf.format(file.lastModified()));
     }
 }
